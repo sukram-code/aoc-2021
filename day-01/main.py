a60ae20f94474c60383a243e1f6be7b631ade8ef
sukram-code <sukram.code@gmail.com>
2021-12-01 22:29:44 +0200

    day 01 - part 2

--- day-01/main.py
+++ day-01/main.py
@@ -10,3 +10,17 @@ for param in radar:
 
 print(depth_counter)
 
+depth_counter = 0
+radar = open('input-b.txt')
+params = [int(entry) for entry in radar.readlines()]
+prev_depth = sum(params[0:3])
+
+for index in range(1, len(params) - 3 + 1):
+    group_depth = 0
+    for group_index in range(3):
+        group_depth = group_depth + params[index + group_index]
+    if group_depth > prev_depth:
+        depth_counter += 1
+    prev_depth = group_depth
+
+print(depth_counter)
