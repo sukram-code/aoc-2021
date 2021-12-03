14eb46db18cbb037ea3ef39db0823543bdee44f8
sukram-code <sukram.code@gmail.com>
2021-12-03 22:22:28 +0200

    day03 done

--- day-03/main.py
+++ day-03/main.py
@@ -1,4 +1,4 @@
-diagnostics = open('input-a.txt')
+diagnostics = open('input.txt')
 bits = [list(diagnostic.rstrip()) for diagnostic in diagnostics.readlines()]
 
 rows = len(bits)
@@ -23,3 +23,42 @@ for column in range(columns):
         epsilon += '0'
 
 print(int(gamma, 2) * int(epsilon, 2))
+
+def search_for_oxygen_rating(bit_arr, pos):
+    if len(bit_arr) == 1:
+        return ''.join(bit_arr[0])
+
+    count_zeroes = 0
+    count_ones = 0
+    for row in range(len(bit_arr)):
+        if bit_arr[row][pos] == '1':
+            count_ones += 1
+        else:
+            count_zeroes += 1
+    if count_ones >= count_zeroes:
+        return search_for_oxygen_rating([bit for bit in bit_arr if bit[pos] == '1'], pos + 1)
+    else:
+        return search_for_oxygen_rating([bit for bit in bit_arr if bit[pos] == '0'], pos + 1)
+
+def search_for_co2_rating(bit_arr, pos):
+    if len(bit_arr) == 1:
+        return ''.join(bit_arr[0])
+
+    count_zeroes = 0
+    count_ones = 0
+    for row in range(len(bit_arr)):
+        if bit_arr[row][pos] == '1':
+            count_ones += 1
+        else:
+            count_zeroes += 1
+    if count_ones >= count_zeroes:
+        filtered_bits = [bit for bit in bit_arr if bit[pos] == '0']
+        return search_for_co2_rating(filtered_bits, pos + 1)
+    else:
+        filtered_bits = [bit for bit in bit_arr if bit[pos] == '1']
+        return search_for_co2_rating(filtered_bits, pos + 1)
+
+oxygen_rating = search_for_oxygen_rating(bits, 0)
+co2_rating = search_for_co2_rating(bits, 0)
+
+print(int(oxygen_rating, 2) * int(co2_rating, 2))
