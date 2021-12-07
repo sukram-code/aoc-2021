27d15b2988683519e545a0bc9c4fc23393230909
sukram-code <sukram.code@gmail.com>
2021-12-07 19:42:13 +0200

    day07 working solution

---
+++ day-07/main.py
@@ -0,0 +1,32 @@
+import sys
+
+crabs = [int(crab) for crab in open('input.txt').read().splitlines()[0].split(',')]
+
+unique_positions = []
+
+for i in range(min(crabs), max(crabs) + 1):
+    unique_positions.append(i)
+
+min_fuel = sys.maxsize
+
+for position in unique_positions:
+    fuel_amount = 0
+    for crab in crabs:
+        fuel_amount += abs(position - crab)
+    if fuel_amount < min_fuel:
+        min_fuel = fuel_amount
+
+print(min_fuel)
+
+min_fuel = sys.maxsize
+
+for position in unique_positions:
+    fuel_amount = 0
+    for crab in crabs:
+        distance = abs(position - crab)
+        for i in range(distance, 0, -1):
+            fuel_amount += i
+    if fuel_amount < min_fuel:
+        min_fuel = fuel_amount
+
+print(min_fuel)
